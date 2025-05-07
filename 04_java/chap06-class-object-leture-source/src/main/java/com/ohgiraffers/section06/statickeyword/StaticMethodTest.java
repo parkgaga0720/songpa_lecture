74fdd91fe3d964b730680265c30316a9edecd49a
parkgaga0720 <parkgaga.0720@gmail.com>
2025-05-07 10:41:54 +0900

    java static

---
+++ 04_java/chap06-class-object-leture-source/src/main/java/com/ohgiraffers/section06/statickeyword/StaticMethodTest.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section06.statickeyword;
+
+public class StaticMethodTest {
+    private int count;
+    public void nonStaticMethod() {
+        this.count++;
+        System.out.println("nonStaticMethod 호출됨...");
+    }
+    public static void staticMethod() {
+        //this,count++;
+        System.out.println("StaticMethod 호출됨...");
+    }
+}
