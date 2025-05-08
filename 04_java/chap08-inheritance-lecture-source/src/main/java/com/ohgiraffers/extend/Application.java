b6ea05811a914d740661ff5b5df309e045bba586
parkgaga0720 <parkgaga.0720@gmail.com>
2025-05-08 12:03:40 +0900

    java 상속

--- 04_java/chap08-inheritance-lecture-source/src/main/java/com/ohgiraffers/extend/Application.java
+++
@@ -1,7 +0,0 @@
-package com.ohgiraffers.extend;
-
-public class Application {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ 04_java/chap08-inheritance-lecture-source/src/main/java/com/ohgiraffers/section02/superkeyword/Product.java
@@ -0,0 +1,77 @@
+package com.ohgiraffers.section02.superkeyword;
+
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+import java.util.Date;
+
+public class Product {
+    private String code; //상품코드
+    private String brand;
+    private String name;
+    private int price;
+    private Date manufacturingDate;
+
+    public Product() {}
+
+    public Product(String code, String brand, String name, int price, Date manufacturingDate) {
+
+        super();
+
+        this.code = code;
+        this.brand = brand;
+        this.name = name;
+        this.price = price;
+        this.manufacturingDate = manufacturingDate;
+
+        System.out.println("Product의 매개변수 있는 생성자 호출함..");
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getManufacturingDate() {
+        return manufacturingDate;
+    }
+
+    public void setManufacturingDate(Date manufacturingDate) {
+        this.manufacturingDate = manufacturingDate;
+    }
+
+    public String getInformation() {
+        return "Product{" +
+                "code='" + code + '\'' +
+                ", brand='" + brand + '\'' +
+                ", name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                ", manufacturingDate=" + manufacturingDate +
+                '}';
+    }
+}
