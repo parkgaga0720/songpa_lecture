bf86930acd5920d55bbdfc8d4650ffbc9714482a
parkgaga0720 <parkgaga.0720@gmail.com>
2025-05-12 14:17:11 +0900

    java Generics

---
+++ 04_java/chap11-generics-leccture-source/src/main/java/com/ohgiraffers/section02/extend/run/Application1.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section02.extend.run;
+
+import com.ohgiraffers.section02.extend.Animal;
+import com.ohgiraffers.section02.extend.RabbitFarm;
+
+public class Application1 {
+    public static void main(String[] args) {
+        //RabbitFarm<Animal> farm1 = new RabbitFarm();
+        RabbitFarm rabbitFarm = new RabbitFarm();
+
+    }
+}
---
+++ 04_java/chap11-generics-leccture-source/src/main/java/com/ohgiraffers/section02/extend/run/Application2.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.section02.extend.run;
+
+public class Application2 {
+    public static void main(String[] args) {
+
+    }
+}
